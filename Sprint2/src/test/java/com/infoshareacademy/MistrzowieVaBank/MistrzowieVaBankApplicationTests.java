57cb56281680090947f2298b4cafe7144cb711ed
Krystyna Slisz <sliszkrystyna@gmail.com>
2022-06-26 14:49:10 +0200

    Sprint1->Sprint2(START)

---
+++ Sprint2/src/test/java/com/infoshareacademy/MistrzowieVaBank/MistrzowieVaBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infoshareacademy.MistrzowieVaBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MistrzowieVaBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
