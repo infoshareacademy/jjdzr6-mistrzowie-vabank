1aa8e7870485ce2c140a5e83dabbc2f8124a5128
Krystyna Slisz <sliszkrystyna@gmail.com>
2022-08-07 13:39:40 +0200

    postmapping in WineController added

--- src/main/java/com/infoshareacademy/mistrzowieVaBank/controller/WineController.java
+++ src/main/java/com/infoshareacademy/mistrzowieVaBank/controller/WineController.java
@@ -6,7 +6,9 @@ import com.infoshareacademy.mistrzowieVaBank.service.WineService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class WineController {
@@ -28,5 +30,11 @@ public class WineController {
     public String getWineForm(Model model) {
         model.addAttribute("wine", new WineDto(null));
         return "wine-form";
+    }
+
+    @PostMapping("/wines/new")
+    public String sendWine(@ModelAttribute("wine") WineDto dto) {
+        System.out.printf(dto.toString());
+        return "wine";
     }
 }
