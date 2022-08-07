e8f8984b4c6d3032e1b3f2cb9e49577c86ded148
Krystyna Slisz <sliszkrystyna@gmail.com>
2022-08-07 12:03:56 +0200

    Cleaning etc

--- Sprint1/src/main/java/com/infoshareacademy/menu/warehouseman/WarehousemanOptioner.java
+++
@@ -1,7 +0,0 @@
-package com.infoshareacademy.menu.warehouseman;
-
-public class WarehousemanOptioner {
-    public void showWarehousemanOption(){
-        System.out.println("Jesteś na profilu magazyniera");
-    }
-}
